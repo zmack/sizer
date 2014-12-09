40a4c0a7a2ea7850bd1e5d50f75f5f4829e209a8
Andrei Bocan <zmaxor@gmail.com>
2014-12-10 01:43:49 +0200

    Barriers

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ use hyper::Url;
 use stb_image::image::LoadResult::{ImageU8,ImageF32,Error};
 use stb_image::image;
 use stb_image::image::{ImageFormat};
+use std::sync::{Arc, Mutex, Barrier};
+use std::vec::Vec;
 
 #[deriving(Show)]
 struct FileStats {
@@ -48,14 +50,30 @@ fn get_file_stats(url: &str) -> Result<FileStats, String> {
 }
 
 fn main() {
-    let url = "http://public.reverbnation.com/Album/12377/album_image/cropped/image.jpg".as_slice();
-    let stats = match get_file_stats(url) {
-        Ok(fs) => fs,
-        Err(s) => {
-            println!("Stupid url {} died because {}", url, s);
-            return
-        }
-    };
+    let list:Arc<Mutex<Vec<FileStats>>> = Arc::new(Mutex::new(Vec::new()));
+    let barrier = Arc::new(Barrier::new(11));
 
-    println!("File is {}", stats);
+    for _ in range(0u, 10u) {
+        let barrier_clone = barrier.clone();
+        let list_clone = list.clone();
+        spawn(proc() {
+            let url = "http://public.reverbnation.com/Album/12377/album_image/cropped/image.jpg".as_slice();
+            match get_file_stats(url) {
+                Ok(fs) => {
+                    list_clone.lock().push(fs);
+                },
+                Err(s) => {
+                    println!("Stupid url {} died because {}", url, s);
+                    return
+                }
+            };
+            barrier_clone.wait();
+        })
+    }
+
+    barrier.wait();
+
+    for thing in list.lock().iter() {
+        println!("List: {}", thing);
+    }
 }
