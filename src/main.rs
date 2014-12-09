2cbf5b0f6170e0b80fce7930131b2ee8e75c7cef
Andrei Bocan <zmaxor@gmail.com>
2014-12-10 01:01:35 +0200

    Rejigger some of this stuff around a bit

--- src/main.rs
+++ src/main.rs
@@ -5,37 +5,57 @@ use hyper::client::Request;
 use hyper::Url;
 use stb_image::image::LoadResult::{ImageU8,ImageF32,Error};
 use stb_image::image;
+use stb_image::image::{ImageFormat};
 
-fn main() {
-    let url = Url::parse("http://public.reverbnation.com/Album/12377/album_image/cropped/image.jpg").unwrap();
-    // let url = Url::parse("http://4.bp.blogspot.com/-IJXe-eEJTmA/U-G6WnOSe_I/AAAAAAAAAtQ/v_V9BcQu9Ok/s33-c/336914de2a.png").unwrap();
+#[deriving(Show)]
+struct FileStats {
+    width: uint,
+    height: uint,
+    size: uint,
+    format: ImageFormat
+}
+
+fn get_file_stats(url: &str) -> Result<FileStats, String> {
+    let url = Url::parse(url).unwrap();
 
     let req = match Request::get(url) {
         Ok(e) => e,
-        _ => return
+        _ => return Err("Could not parse url".to_string())
     };
 
     let mut response = req.start().unwrap().send().unwrap();
     let buf = match response.read_to_end() {
-        Err(_) => return,
+        Err(_) => return Err("Could not read buffer".to_string()),
         Ok(a) => a
     };
-
-    println!("File size was {}", buf.len());
-
 
     let image = match image::load_from_memory(buf.as_slice()) {
         ImageU8(z) => z,
         ImageF32(_) => {
-            println!("F32!");
-            return;
+            return Err("Weird image so we're bailing".to_string());
         },
-        Error(s) => { println!("Bailed because {}", s); return }
+        Error(s) => { return Err(s) }
     };
 
     let image_type = image::get_image_format(buf.as_slice());
-    println!("Image type is {}", image_type);
 
-    println!("File is {} long", buf.len());
-    println!("Image is {}x{}", image.width, image.height);
+    Ok(FileStats {
+        size: buf.len(),
+        width: image.width,
+        height: image.height,
+        format: image_type,
+    })
+}
+
+fn main() {
+    let url = "http://public.reverbnation.com/Album/12377/album_image/cropped/image.jpg".as_slice();
+    let stats = match get_file_stats(url) {
+        Ok(fs) => fs,
+        Err(s) => {
+            println!("Stupid url {} died because {}", url, s);
+            return
+        }
+    };
+
+    println!("File is {}", stats);
 }
